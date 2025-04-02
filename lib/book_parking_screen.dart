072a8174ff35faf92a9c103a8e40f0f1595183e8
nnn <sajaawad2003@gmail.com>
2025-04-03 00:56:59 +0300

    New commits :))

--- lib/book_parking_screen.dart
+++ lib/book_parking_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
 
+
 class BookParkingScreen extends StatefulWidget {
   const BookParkingScreen({super.key});
 
@@ -9,6 +10,9 @@ class BookParkingScreen extends StatefulWidget {
 }
 
 class _BookParkingScreenState extends State<BookParkingScreen> {
+  CalendarFormat _calendarFormat = CalendarFormat.month;
+  DateTime _focusedDay = DateTime.now();
+  DateTime? _selectedDay;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,9 +22,42 @@ class _BookParkingScreenState extends State<BookParkingScreen> {
         leading: Icon(Icons.arrow_back),
         title: Text("Book Parking Details", style: TextStyle(fontWeight: FontWeight.bold),),
       ),
-      body: Column(
-
-      ),
-    );
+      body: Center(
+        child: Column(
+          children: [
+            TableCalendar(
+              firstDay: DateTime.now(),
+              lastDay: DateTime.utc(2030, 12, 31),
+              focusedDay: _focusedDay,
+              selectedDayPredicate: (day) => isSameDay(_selectedDay, day),
+              onDaySelected: (selectedDay, focusedDay) {
+                setState(() {
+                  _selectedDay = selectedDay;
+                  _focusedDay = focusedDay;
+                });
+              },
+              calendarFormat: _calendarFormat,
+              onFormatChanged: (format) {
+                setState(() {
+                  _calendarFormat = format;
+                });
+              },
+              calendarStyle: CalendarStyle(
+                todayDecoration: BoxDecoration(
+                  color: Colors.pink.shade100,
+                  shape: BoxShape.circle,
+                ),
+                selectedDecoration: BoxDecoration(
+                  color: Color(0xFFFF5177),
+                  shape: BoxShape.circle,
+                ),
+              ),
+            ),
+          ],
+        ),
+        ),
+      );
   }
 }
+
+
